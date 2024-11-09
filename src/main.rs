bc64fb0f50cc9ed7654b119ad6ac4d4b9655ca95
Cameron Howell <jango.djarin@proton.me>
2024-11-08 22:08:12 -0600

    Got it to build

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use iced::system;
+mod screen;
+
+use crate::screen::help;
+use crate::screen::Screen;
+
 use iced::{Element, Subscription, Task, Theme};
 
 pub fn main() -> iced::Result {
     iced::application(Centurion::title, Centurion::update, Centurion::view)
-        .font(icon::FONT_BYTES)
         .subscription(Centurion::subscription)
         .theme(Centurion::theme)
-        .run_with(Centurion::new)
+        .run_with(move || Centurion::new())
 }
 
 struct Centurion {
@@ -14,15 +17,18 @@ struct Centurion {
 }
 
 #[derive(Debug, Clone)]
-enum Message {
-    Escape,
+pub enum Message {
+    Help(help::Message),
 }
 
 impl Centurion {
-    pub fn new() -> Self {
-        Self {
-            screen: Screen::Loading,
-        }
+    pub fn new() -> (Self, Task<Message>) {
+        (
+            Self {
+                screen: Screen::Help(help::Help::new(true)),
+            },
+            Task::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -31,7 +37,31 @@ impl Centurion {
 
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::Escape => Task::none(),
+            Message::Help(_message) => {
+                let Screen::Help(_help) = &mut self.screen else {
+                    return Task::none();
+                };
+
+                Task::none()
+            }
         }
+    }
+
+    fn view(&self) -> Element<Message> {
+        match &self.screen {
+            Screen::Help(help) => help.view().map(Message::Help),
+        }
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        let screen = match &self.screen {
+            Screen::Help(_help) => Subscription::none(),
+        };
+
+        Subscription::batch([screen])
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::TokyoNight
     }
 }
